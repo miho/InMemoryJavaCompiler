09478c1ba999dbf2dc4c1e8ea3864a5fc5df04d1
Michael Hoffer <info@michaelhoffer.de>
2020-03-10 15:04:55 +0100

    license info and publishing info

--- src/main/java/eu/mihosoft/jcompiler/LangUtils.java
+++ src/main/java/eu/mihosoft/jcompiler/LangUtils.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2020 Michael Hoffer <info@michaelhoffer.de>. All rights reserved.
+ *
+ * Code is based on abandoned project by Trung (https://github.com/trung/InMemoryJavaCompiler).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package eu.mihosoft.jcompiler;
 
 import java.util.ArrayList;
@@ -7,7 +24,7 @@ import java.util.regex.Pattern;
 
 
 /**
- * Language utilities.
+ * Language utilities based on VRLs VLangUtils.
  * 
  * @author Michael Hoffer &lt;info@michaelhoffer.de&gt;
  */
